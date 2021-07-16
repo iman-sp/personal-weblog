29c5d30ee170f80cc22a25a084667743a7523f3d
iman_sp <iman_sp@yahoo.com>
2021-07-16 14:28:32 +0430

    change Routing setting

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    //uncomment below line for old routing
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
