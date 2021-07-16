7b17f0497a3d1ca1781b7cf1984bbd235d2fb3b4
iman_sp <iman_sp@yahoo.com>
2021-07-16 23:42:51 +0430

    develop header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,16 +6,40 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel = "icon" href="images/favicon.ico" type = "image/x-icon">
-
+    <link rel="stylesheet" href="css/globalStyle.css">
+    <link rel="stylesheet" href="css/header.css">
+    @yield('css')
 
 
 </head>
 <body>
-    <div id="app">
 
-        <main class="py-4">
+
+
+
+    <div id="app">
+        <header id="header">
+            <nav id="navbar">
+                <div>
+                    <div>
+                        <a id="aboutme" class="white" href="{{ route('aboutme') }}">درباره من</a>
+
+                    </div>
+                </div>
+            </nav>
+            <div>
+                <span class="white weblogtitle">وبلاگ ایمان</span>
+                <p class="white">به وبلاگ من خوش اومدید </p>
+            </div>
+        </header>
+
+        <main>
             @yield('content')
+
         </main>
+        <footer>
+            this is footer
+        </footer>
     </div>
 </body>
 
