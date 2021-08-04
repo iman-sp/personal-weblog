cd042e3c2747cceac9fabb4147a78367ce5a9962
iman_sp <iman_sp@yahoo.com>
2021-08-05 01:24:58 +0430

    completing about page

---
+++ resources/views/About.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title' , 'درباره من')
+@section('content')
+    @section('css')
+        <link rel="stylesheet" href="css/about.css">
+    @endsection
+
+
+<div id="aboutmebox">
+    <h5>سلام ، این متن برای تست است .</h5>
+    <p> این بخش هنوز کامل نشده  </p>
+    <a href="https://www.instagram.com/iman330sp/">instagram</a>
+    <br>
+    <a href="https://twitter.com/iman330sp">twitter</a>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum numquam optio consequatur harum, nemo dolorem exercitationem quidem repellat quo. Modi aut pariatur quos vero cum, expedita delectus omnis quisquam repellendus!</p>
+</div>
+@endsection
