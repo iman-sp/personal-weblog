f3c90a3eeaafff5dfea8339829b7d68b3d9ea047
iman_sp <iman_sp@yahoo.com>
2021-08-05 02:14:54 +0430

    completing index page

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.app')
 @section('title' , 'صفحه اصلی وبلاگ ایمان')
+    @section('css')
+        <link rel="stylesheet" href="css/index.css">
+    @endsection
 @section('content')
-<h1>this is index page where show all posts</h1>
+
+    <div class="index-main">
+        <section class="posts">
+            <div class="posts-padding">
+
+
+
+                    {{-- @foreach($posts as $item) --}}
+                    <a href="{{ route('post') }}">
+                        <div class="index-post">
+                            <div class="main-image">
+                                <img src="/images/posts/future.jpg" alt="">
+                            </div>
+
+                            <div class="title-post">
+                                <p>
+                                    عنوان مطلب
+                                </p>
+                            </div>
+
+                            <div class="index-line"></div>
+
+                            <div class="description">
+                                <p>
+                                    توضیح کوتای درباره مطلب
+                                </p>
+                            </div>
+
+                        </div>
+                    </a>
+
+                {{-- @endforeach --}}
+
+
+            </div>
+        </section>
+    </div>
+{{-- @endsection --}}
+
+
 @endsection
