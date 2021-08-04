9745ebcdf5c9225f4f38664f573646d66206cdc5
iman_sp <iman_sp@yahoo.com>
2021-08-05 03:16:41 +0430

    completing post page

--- resources/views/Post.blade.php
+++ resources/views/Post.blade.php
@@ -1,1 +1,77 @@
-<h1>post gaem</h1>
+@extends('layouts.app')
+@section('title' , 'title show here')
+    @section('css')
+        <link rel="stylesheet" href="css/post.css">
+    @endsection
+@section('content')
+
+
+
+
+
+
+
+
+<style>
+    header{
+        height: 220px!important;
+    }
+</style>
+
+
+
+    <div id="product-main">
+        <div  style="width:100%;margin:auto">
+
+
+            <section id="post">
+
+
+                <div id="title">
+                    <h1>حریم خصوصی در اینترنت</h1>
+                </div>
+
+
+
+                <div id="post-image" style="margin-bottom:60px">
+                    <div id="post-image-box">
+                        <img src="images/posts/future.jpg" alt="alt goes here">
+                    </div>
+                </div>
+
+
+
+
+
+                <div id="post-caption" class="fr-view">
+                    <p>
+                        سلام امروز میخوام درباره حریم خصوصی توی اینترنت صحبت کنم .
+                        این روز ها که تمام کار های ما با اینترنت در ارتباط هست ،‌ باید خیلی بیشتر مراقب چیز هایی باشیم که در اینترنت منتشر میکنید باشیم .
+                    </p>
+
+                </div>
+
+
+
+
+
+
+
+
+            </section>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
